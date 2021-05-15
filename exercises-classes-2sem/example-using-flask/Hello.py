6c8426d197b3e2d638f5cf214f9264af08631919
jenifer-mathias <jenifer.santos@zup.com.br>
2021-05-15 19:19:39 -0300

    Web text rendering using Flask microframework
    Signed-off-by: jenifer-mathias <jenifer.santos@zup.com.br>

---
+++ exercises-classes-2sem/example-using-flask/Hello.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return '<h1>Hello!</h1>'
+
+
+@app.route('/<name>')
+def name(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
