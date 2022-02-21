df87462e0b55446a3b02c399a6a918c2d7e2f416
jenifer-mathias <jenifer.mathias.santos@hotmail.com>
2022-02-21 20:52:53 -0300

    Code adjustment to aplly 'long long' type
    Signed-off-by: jenifer-mathias <jenifer.mathias.santos@hotmail.com>

--- c-exercises/fibonacci-memoization/Fibonacci.c
+++ c-exercises/fibonacci-memoization/Fibonacci.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 
-int fib[99];      // variável global para armazenar os valores de fib
-
-int fibonacci(int numero) {
-    if(numero == 1 || numero == 0) {
-        return numero;
-    }
-    if(fib[numero] != -1) {
-        return fib[numero];
-    }
-    fib[numero] = fibonacci(numero - 1) + fibonacci(numero - 2);
-    return fib[numero];
-}
+long long int fib[99];      // variável global para armazenar os valores de fib
 
 void memoizacaoNumeros() {
     for (int i = 0; i < 99; ++i) {
@@ -19,12 +8,17 @@ void memoizacaoNumeros() {
     }
 }
 
+long long int fibonacci(int numero) {
+    if(numero == 1 || numero == 0) return numero;
+    if(fib[numero] != -1) return fib[numero];
+    fib[numero] = fibonacci(numero - 1) + fibonacci(numero - 2);
+    return fib[numero];
+}
 int main() {
-    memoizacaoNumeros(); // aplicando a técnica de memoização
-
     printf("**** Fibonacci usando a técnica de memoização ****");
     int numero;
     printf("\nDigite um número inteiro para obter o Fibonacci deste número: ");
     scanf("%i", &numero);
-    printf("O Fibonacci de %i é: %i", numero, fibonacci(numero));
+    memoizacaoNumeros(); // aplicando a técnica de memoização
+    printf("O Fibonacci de %i é: %lli", numero, fibonacci(numero));
 }
